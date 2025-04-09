57cfd4b98db8446988f475fa8f2c3de7ae29a55a
Le Huynh Thai <154292516+raikouxyz@users.noreply.github.com>
2025-04-09 10:26:08 +0700

    chat
    update

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,11 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('posts.index') }}">
-            Mạng xã hội
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
         
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto">
