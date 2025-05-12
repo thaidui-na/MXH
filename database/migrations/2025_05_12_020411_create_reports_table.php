ab51ca249512e9d33b9c24d9d06b8f76d547107f
hung957 <hungphuc957@gmail.com>
2025-05-12 10:45:05 +0700

    Bao cao bai viet

--- database/migrations/2025_05_12_020411_create_reports_table.php
+++ database/migrations/2025_05_12_020411_create_reports_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->text('reason');
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            // $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
         });
     }
 
