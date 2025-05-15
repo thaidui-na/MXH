a93d81385f6b06d72940a69ea82c75cece6eda1f
DucPhamQuang <22211tt0650@mail.tdc.edu.vn>
2025-05-15 23:30:48 +0700

    gửi lời mời kết bạn Phạm Quang Đức

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,9 +20,19 @@ class UserController extends Controller
                         ->orWhere('email', 'like', "%{$query}%");
                 });
             })
+            ->where('id', '!=', auth()->id())
             ->select(['id', 'name', 'email', 'avatar'])
             ->limit(10)
-            ->get();
+            ->get()
+            ->map(function($user) {
+                return [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'avatar' => $user->avatar,
+                    'isFriend' => auth()->user()->isFollowing($user)
+                ];
+            });
 
         return response()->json([
             'users' => $users
@@ -50,5 +60,59 @@ class UserController extends Controller
             'users' => $users,
             'query' => $query
         ]);
+    }
+
+    public function addFriend(User $user)
+    {
+        if (auth()->id() === $user->id) {
+            return response()->json(['error' => 'Bạn không thể kết bạn với chính mình'], 400);
+        }
+
+        $isFriend = auth()->user()->isFollowing($user);
+        
+        if ($isFriend) {
+            return response()->json(['error' => 'Bạn đã là bạn bè với người dùng này rồi'], 400);
+        }
+
+        auth()->user()->following()->attach($user->id);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Đã gửi lời mời kết bạn thành công',
+            'isFriend' => true
+        ]);
+    }
+
+    public function removeFriend(User $user)
+    {
+        if (auth()->id() === $user->id) {
+            return response()->json(['error' => 'Bạn không thể hủy kết bạn với chính mình'], 400);
+        }
+
+        $isFriend = auth()->user()->isFollowing($user);
+        
+        if (!$isFriend) {
+            return response()->json(['error' => 'Bạn chưa là bạn bè với người dùng này'], 400);
+        }
+
+        auth()->user()->following()->detach($user->id);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Đã hủy kết bạn thành công',
+            'isFriend' => false
+        ]);
+    }
+
+    public function followers(User $user)
+    {
+        $followers = $user->followers()->paginate(20);
+        return view('users.followers', compact('user', 'followers'));
+    }
+
+    public function following(User $user)
+    {
+        $following = $user->following()->paginate(20);
+        return view('users.following', compact('user', 'following'));
     }
 }
